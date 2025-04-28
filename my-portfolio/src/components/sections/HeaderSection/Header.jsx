efe1afcb54c6fda50234d656041145bdd29fb6a6
iamfannyhenriques <fanny.henriques@hotmail.com>
2025-04-28 17:48:24 +0200

    changed order of projects

--- my-portfolio/src/components/sections/HeaderSection/Header.jsx
+++ my-portfolio/src/components/sections/HeaderSection/Header.jsx
@@ -5,18 +5,18 @@ import "./Header.css";
 
 export const Header = () => {
   return (
-  <div className="header"> 
-    <section className="header-container">
-      <div className="header-section">
-        <H3> Hi there, I’m</H3>
-        <H1>Fanny Henriques</H1>
-        <H3>
-          Creative Front-End Developer with a background in social science and social work
-        </H3>
-      </div>
-    </section>
-    <Gallery/>
-    <Bio/>
-  </div>
+    <div className="header">
+      <section className="header-container">
+        <div className="header-section">
+          <H3> Hi there, I’m</H3>
+          <H1>Fanny Henriques</H1>
+          <H3>
+            Creative Junior Developer with a background in social science and social work
+          </H3>
+        </div>
+      </section>
+      <Gallery />
+      <Bio />
+    </div>
   );
 };
--- my-portfolio/src/components/sections/LetsTalkSection/LetsTalk.jsx
+++ my-portfolio/src/components/sections/LetsTalkSection/LetsTalk.jsx
@@ -1,20 +1,20 @@
 import "./LetsTalk.css";
 import profileImg from "../../../assets/profile-img.jpg";
-import { H2, BodyTextLarge } from "../../ui/Typography/Typography";
+import { H2, BodyTextMedium } from "../../ui/Typography/Typography";
 import { IconButton } from "../../ui/Buttons/IconButtons";
 
 export const LetsTalk = () => {
 
   const iconData = [
-    {
-      id: 1, iconName: "linkedin", onClick: () => window.open("https://www.linkedin.com", "_blank")
-    },
+    // {
+    //   id: 1, iconName: "linkedin", onClick: () => window.open("https://www.linkedin.com", "_blank")
+    // },
     {
       id: 2, iconName: "github", onClick: () => window.open("https://github.com/Fannyhenriques", "_blank")
     },
-    {
-      id: 3, iconName: "stackOverflow", onClick: () => window.open("https://stackoverflow.com", "_blank")
-    },
+    // {
+    //   id: 3, iconName: "stackOverflow", onClick: () => window.open("https://stackoverflow.com", "_blank")
+    // },
     // {
     //   id: 4, iconName: "twitter", onClick: () => window.open("https://twitter.com", "_blank")
     // },
@@ -29,9 +29,9 @@ export const LetsTalk = () => {
         <H2>Lets talk</H2>
         <img className="profile-img" src={profileImg} alt="profile picture" />
         <div className="contact-section">
-          <BodyTextLarge>Fanny Henriques</BodyTextLarge>
-          <BodyTextLarge>+46(0)73 618 13 73</BodyTextLarge>
-          <BodyTextLarge>fanny.henriques@hotmail.com</BodyTextLarge>
+          <BodyTextMedium>Fanny Henriques</BodyTextMedium>
+          <BodyTextMedium>+46(0)73 618 13 73</BodyTextMedium>
+          <BodyTextMedium>fanny.henriques@hotmail.com</BodyTextMedium>
         </div>
         <div className="icons-container">
           {iconData.map(({ id, iconName, onClick }) => (
